388c283b19af59fb920420072d351197f77a7038
adrien <castella.adrien@gmail.com>
2020-12-09 11:14:00 +0100

    Fixed errors

--- Modelling.py
+++ Modelling.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Modelling:
-    def __init__(self, init_grid=np.array([[0]])):
+    def __init__(self, init_grid=np.array([[0]]), c=0, h=0, dt=1):
         self.c = c
         self.h = h
         self.dt = dt
@@ -9,31 +9,33 @@ class Modelling:
     
     def next_grid(self, k):
         # compute grid in next time step (nested for loop)
-        ngrid = 0 # initialize the next grid?
+        ngrid = np.array([[0]]) # initialize the next grid?
 
-        for i in range(0, length(self.grid[k])):
-            for j in range(0, length(self.grid[k][i])):
+        for i in range(0, len(self.grid[k])):
+            for j in range(0, len(self.grid[k][i])):
                 # use the "update formula"
                 print("hello")
-        return ngrid
+        return np.array([ngrid])
     
     def solveEq(self):
         # for loop for time steps and updating grid
         k = 0
 
-        while(True): # some stopping condition
+        while(k < 10): # some stopping condition
             # use next_grid to compute next step
+            ngrid = self.next_grid(k)
+            print(self.grid)
+            print(type(ngrid))
             self.grid = np.append(self.grid,
-                        np.array[next_grid(k)],
+                        ngrid,
                         axis = 0)
             print(k) # not needed
             k = k + 1
-    
-    def set_params(self, c = self.c, h = self.h, dt = self.dt):
-        self.c = c
-        self.h = h
-        self.dt = dt
-    
+
+    #def set_param(self, c = self.c, h = self.h, dt = self.dt):
+    #    self.c = c
+    #    self.h = h
+    #    self.dt = dt
 
 instance = Modelling()
 instance.solveEq()
