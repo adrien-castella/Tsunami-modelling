23ba214357f5de27f2175793f891aeecd0fcddaf
adrien <castella.adrien@gmail.com>
2020-12-09 11:29:38 +0100

    Added comments

--- Modelling.py
+++ Modelling.py
@@ -5,6 +5,7 @@ class Modelling:
         self.c = c
         self.h = h
         self.dt = dt
+        # add grid at time = 0
         self.grid = np.array([init_grid])
     
     def next_grid(self, k):
@@ -17,11 +18,11 @@ class Modelling:
                 print("hello")
         return np.array([ngrid])
     
-    def solveEq(self):
+    def solveEq(self, max):
         # for loop for time steps and updating grid
         k = 0
 
-        while(k < 10): # some stopping condition
+        while(k < max): # some stopping condition
             # use next_grid to compute next step
             ngrid = self.next_grid(k)
             print(self.grid)
@@ -33,6 +34,7 @@ class Modelling:
             k = k + 1
         print("hello123")
     
+    # set the initial parameters
     def set_param(self, c = 0, h = 0, dt = 0):
         if not c == 0:
             self.c = c
@@ -41,15 +43,33 @@ class Modelling:
         if not dt == 0:
             self.dt = dt
     
-    def check_stability(self):
+    def check_stability(self): # add given parameters
+        # check whether problem is stable with given parameters
         print("your life is not stable")
 
     def min_stability(self):
+        # find minimal stable parameters, whatever that may mean
         for i in range(0,10):
             self.check_stability()
             print("test this for stability")
 
-instance = Modelling()
-instance.set_param(h = 1)
-instance.solveEq()
+class Grid:
+    def __init__(self):
+        # class structure for Grids, might be helpful
+        # could improve computational time
+
+
+# create grid
+grid = Grid()
+
+# create instance of modelling class
+instance = Modelling() # need to give the grid here like (grid)
+
+# find minimum stability conditions
 instance.min_stability()
+
+# set the parameters
+instance.set_param(h = 1) # can add c = ?, h = ?, dt = ?
+
+# solve the wave equations numerically
+instance.solveEq() # should take as parameter max time
